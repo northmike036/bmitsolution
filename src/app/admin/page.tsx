950adc38281f52710a464f2330ce9f19c81fd8a4
Yeasir Ahamed <yahamed007@gmail.com>
2025-12-11 05:56:37 -0700

    change file

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,8 +2,8 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import AnalyticContainer from "@/components/admin/AdminSummary";
 import AnalyticsDashboard from "@/components/admin/AdminSummaryV2";
+import AnalyticWrapper from "@/components/admin/AdminAnalyticsWrapper";
 
 export default async function AdminPage() {
   const session = await getServerSession(authOptions);
@@ -12,8 +12,8 @@ export default async function AdminPage() {
   }
 
   return (
-    <AnalyticContainer>
+    <AnalyticWrapper>
       <AnalyticsDashboard />
-    </AnalyticContainer>
+    </AnalyticWrapper>
   );
 }
